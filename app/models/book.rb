aba85b0fac54a413310f7452c099d2428da77800
Khanh Nguyen <knguyen77@academic.rrc.ca>
2022-10-10 15:15:05 -0500

    Added validations to models

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,5 @@ class Book < ApplicationRecord
   has_many :subjects, through: :book_subjects
   has_many :book_publishers
   has_many :publishers, through: :book_publishers
+  validates :title, :price, :pages, presence: true
 end
