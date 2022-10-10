24ee96c918282c90467e8641b30e4c946b002734
Khanh Nguyen <knguyen77@academic.rrc.ca>
2022-10-10 15:02:20 -0500

    Fetch author key from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+require "rest-client"
+require "json"
+
+filename = Rails.root.join("db/authors.csv")
+
+csv_data = File.read(filename)
+authors = CSV.parse(csv_data, headers:true, encoding: "utf-8")
+
+authors.each do |a|
+  author_url = "https://openlibrary.org" + a["key"] + "/works.json"
+
+  result = RestClient.get(author_url)
+
+end
+
