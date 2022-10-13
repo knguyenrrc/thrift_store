1ec9739372e1a154692c08a45e11a510b2a29f15
Khanh Nguyen <knguyen77@academic.rrc.ca>
2022-10-13 01:38:41 -0500

    Updated Subject and Publisher model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,5 @@
 class Subject < ApplicationRecord
   has_many :book_subjects
-  has_many :books, :through => :book_subjects
+  has_many :books, through: :book_subjects
   validates :name, presence: true
 end
